582ea0db7fe257cbadcd6b6609b46d11057d47ad
Tecnocete <102417749+Tecnocete@users.noreply.github.com>
2024-07-04 18:09:32 +0200

    Correción

--- ejercicio13.py
+++ ejercicio13.py
@@ -33,4 +33,5 @@ while True:
             print("No coinciden las contraseñas, vuelva a introducir la contraseña de nuevo") 
             contraseña 
     else:
+        print("La contraseña debe contener al menos una minuscula una mayuscula y un digito")
         contraseña       
